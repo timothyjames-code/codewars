3f1a3f688f1e095dbe321e86a0864afd7ba55bf3
Tim <Ti.corwin229@gmail.com>
2023-09-17 18:19:27 -0400

    2nd code wars 9/17/2023

---
+++ lost-without-a-map.js
@@ -0,0 +1,15 @@
+DESCRIPTION:
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// ************My Solution**********
+
+function maps(x){
+    
+  let newMap =  x.map((val) => val * 2);
+  
+  return newMap
+}
