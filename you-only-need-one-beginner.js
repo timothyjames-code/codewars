5618ba66940f8a4a885b79bf8beece9a5baaacdd
Tim <Ti.corwin229@gmail.com>
2023-09-19 12:06:23 -0400

    new code wars

---
+++ you-only-need-one-beginner.js
@@ -0,0 +1,24 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+
+// ************My Solution**********
+
+  
+  function simpleMultiplication(number){
+    // your code........
+    
+    function check(a, x) {
+      // your code here
+    return a.includes(x)
+    }
+
+}
+
+
+  // *****BETTER SOLUTION*****
+
+  // const check = (a,x) => a.includes(x);
