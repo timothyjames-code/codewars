6944ec24c6a29e47a38211291028783189f46350
Tim <Ti.corwin229@gmail.com>
2023-09-19 12:17:32 -0400

    more code wars 9/19/23

---
+++ convert-a-string-to-an-array.js
@@ -0,0 +1,10 @@
+// Write a function to split a string and convert it into an array of words.'t be a space at the beginning or the end of the sentence!
+
+  
+function stringToArray(string){
+
+	return string.split(' ')
+
+}
+
+
