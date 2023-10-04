8bbe419a929803da205a451334f824fb61c3b502
timothyjames-code <110254007+timothyjames-code@users.noreply.github.com>
2023-10-04 18:50:34 -0400

    returns message based on parameter's

---
+++ grasshopper-personalized-message.js
@@ -0,0 +1,13 @@
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+// Use conditionals to return the proper message:
+
+// case	return
+// name equals owner	'Hello boss'
+// otherwise	'Hello guest'
+
+// ************My Solution**********
+
+  
+let greet = (name, owner) =>
+   name === owner ? 'Hello boss' : 'Hello guest'
