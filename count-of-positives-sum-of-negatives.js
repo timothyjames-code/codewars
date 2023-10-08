c32066ba70935b3ded9c5e894687bbb60051b872
timothyjames-code <110254007+timothyjames-code@users.noreply.github.com>
2023-10-08 19:19:42 -0400

    counts positive nums and sums negative nums in arr

---
+++ count-of-positives-sum-of-negatives.js
@@ -0,0 +1,33 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+// ************My Solution**********
+
+  
+function countPositivesSumNegatives(input) {
+  
+  if (input === null || input.length ===0){
+    return []
+  }
+  
+  let arr = []
+  
+  let positiveNum = input.filter((num)=> num > 0)
+  let negativeNum = input.filter((num) => num < 0)
+
+  
+  let minus = negativeNum.reduce((acc,c)=> acc + c,0)
+  
+  arr.push(positiveNum.length);
+  arr.push(minus);
+  
+  return arr
+}
+
+
