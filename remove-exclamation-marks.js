04919ebda4eff1e291374a90ebff324929ab9d8c
timothyjames-code <110254007+timothyjames-code@users.noreply.github.com>
2023-10-22 22:08:18 -0400

    removes exclamations from a string

---
+++ remove-exclamation-marks.js
@@ -0,0 +1,9 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+// ************My Solution**********
+
+function removeExclamationMarks(s) {
+  let newStr = s.replaceAll("!", "");
+
+  return newStr;
+}
